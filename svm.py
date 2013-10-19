f20bdf6d0592476898f0cb40672066d8e3c157a3
Arryon Tijsma <arryon@gmail.com>
2013-10-19 15:22:43 +0200

    svn werkend

--- svm.py
+++ svm.py
@@ -2,6 +2,7 @@ import sys
 from sklearn import svm
 from sklearn import cross_validation
 from sklearn import datasets
+from sklearn import grid_search
 
 import matplotlib.pyplot as plt
 
@@ -10,16 +11,29 @@ import numpy as np
 target = np.load('target.npy')
 database = np.load('database.npy')
 
-print database
+data = list()
+for d in database:
+    new = [float(e) for e in d]
+    data.append(new)
 
-classifiers = ['rbf', 'linear', 'poly']
+newtarget = list()
+for t in target:
+    newtarget.append(int(t))
+
+classifiers = ['linear','rbf','sigmoid','poly']
 
 def error_with_classifier():
     for c in classifiers:
-        clf = svm.SVC(c) 
-        cv = cross_validation.ShuffleSplit(n=len(target), n_iter=10,test_size=0.33, random_state=0)
-        scores = cross_validation.cross_val_score(clf, np.array(database), np.array(target), cv=cv)
-        return (c, scores.mean(), scores.std())
+        clf = svm.SVC(kernel=c) 
+            
+        cv = cross_validation.ShuffleSplit(n=len(newtarget), n_iter=10,test_size=0.1, random_state=0)
+        param_grid = [
+          {'C': [1, 10, 100, 1000], 'kernel': ['linear'], 'degree':[2,3,4,5]}
+         ]
+        out = grid_search.GridSearchCV(clf, param_grid=param_grid, cv=cv)
+        
+        #scores = cross_validation.cross_val_score(clf, np.array(data), np.array(newtarget), cv=cv)
+        #print (c, scores.mean(), scores.std())
 
 
 
